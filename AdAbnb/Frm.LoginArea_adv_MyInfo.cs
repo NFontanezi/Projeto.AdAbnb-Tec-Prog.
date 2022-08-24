58d1d82df972f12ac9bb12f0db7551e466813dbd
NFontanezi <104529547+NFontanezi@users.noreply.github.com>
2022-08-24 09:54:32 -0300

    updatin login &  MyInfo

---
+++ AdAbnb/Frm.LoginArea_adv_MyInfo.cs
@@ -0,0 +1,51 @@
+﻿using AdAbnb.Domain;
+using AdAbnb.Repositories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdAbnb.Presentation
+{
+    public partial class frmLoginAreaAdv_MyInfo : Form
+    {
+        Owner Owner { get; set; }
+        public frmLoginAreaAdv_MyInfo(Owner owner)
+        {
+            Owner = owner;
+            InitializeComponent();
+        }
+
+        private void frmLoginAreaAdv_MyInfo_Load(object sender, EventArgs e)
+        {
+            lblMyInfo_adv.Text = ToString1(Owner);
+        }
+
+        public static string ToString1(Owner owner)
+        {
+            return $@"
+    Nome: {owner.Name}
+    CPF: {owner.CPF}
+    Telefone: {owner.Phone}   
+    Data de nascimento: {owner.DateBirth}
+    Gênero: {owner.Gender}
+    Banco: {owner.Bank}
+    Agência: {owner.BankAgency}
+    Conta: {owner.BankAccount}
+    Data de nascimento: {owner.DateBirth}";
+        }
+
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            var t = new Thread(() => Application.Run(new frmAdvArea(Owner)));
+            this.Close();
+            t.Start();
+        }
+    }
+}
